e9a3f4823b55042ece7bd3e39033edc6e1a2d8f1
Abhishek-Mukherjee96 <abhishekmukherjee.dgp@gmail.com>
2020-05-26 17:17:25 +0530

    published my first website
    i am very much happy

---
+++ js/main.js
@@ -0,0 +1,48 @@
+//star typed
+  const typedTextSpan=document.querySelector(".typed-text");
+
+  const textArray=["Web Developer", "Web Designer", "Frontend Developer", "Graphic Designer"];
+  const typingDelay=200;
+  const erasingDelay=100;
+  const newTextDelay=2000;
+  let textArrayIndex=0;
+  let charIndex=0;
+
+  function type(){
+  	if(charIndex < textArray[textArrayIndex].length){
+  		typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+  		charIndex++;
+  		setTimeout(type, typingDelay);
+  	}
+  	else{
+  		setTimeout(erase, newTextDelay);
+
+  	}
+  }
+
+  function erase(){
+  	if(charIndex > 0){
+  		typedTextSpan.textContent = textArray[textArrayIndex].substring(0,charIndex-1);
+  		charIndex--;
+  		setTimeout(erase, erasingDelay);
+  	}
+  	else{
+  		textArrayIndex++;
+  		if(textArrayIndex>=textArray.length) textArrayIndex=0;
+  		setTimeout(type, typingDelay + 1100);
+  	}
+  }
+ 
+  document.addEventListener("DOMContentLoaded", function() {
+  	 setTimeout(type, newTextDelay + 250);
+
+  });
+
+  //jQuery to collapse the navbar on scroll
+  $(window).scroll(function() {
+    if ($(".navbar-default").offset().top > 50) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+  });
